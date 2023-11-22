38cb6f59399e5dea1d13c243dbd108ab81082334
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-11-22 16:41:39 -0500

    Update number _guess.sql

--- number _guess.sql
+++ number _guess.sql
@@ -1,3 +1,18 @@
+
+psql --username=freecodecamp --dbname=postgres
+#Create a number_guess database to hold the information suggested in the user stories
+CREATE DATABASE number_guess
+\c number_guess
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(22) NOT NULL UNIQUE
+);
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    number_of_attempts INT NOT NULL,
+    user_id INT REFERENCES users(user_id)
+);
+
 #Create a number_guessing_game folder in the project folder for your program
 mkdir number_guessing_game
 touch number_guess.sh
