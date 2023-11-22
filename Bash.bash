17702fc7f0d8a650d42c22f897dc33f4874b3d68
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-11-22 16:14:34 -0500

    Update Bash.bash

--- Bash.bash
+++ Bash.bash
@@ -1,1 +1,16 @@
-
+#Create a number_guessing_game folder in the project folder for your program
+mkdir number_guessing_game
+touch number_guess.sh
+chmod +x number_guess.sh
+git int
+git add .
+git commit -m "Initial commit"
+git checkout -b main
+git add .
+git commit -m "feat: Inserting username into db"
+git add .
+git commit -m "fix: fixing if statement conditionals"
+git add .
+git commit -m "feat: adding random number variable"
+git add .
+git commit -m "test: testing script"
